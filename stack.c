5bc9acbee56954a660fcdf5d4ffbb19ce6c78538
brndmgd <brndmgdwastaken@gmail.com>
2025-03-04 20:00:41 +0600

    feat: stack

---
+++ stack.c
@@ -0,0 +1,46 @@
+#include "stack.h"
+#include <stdbool.h>
+#include <stdlib.h>
+#define STACK_UNDERFLOW -100
+#define STACK_OVERFLOW -101
+
+stack *stack_init(int max_size) {
+  stack *st = (stack *)malloc(sizeof(stack));
+  st->data = malloc(sizeof(int) * max_size);
+  st->max_size = max_size-1;
+  st->size = -1;
+  return st;
+}
+
+int push(stack *st, int value) {
+  if (st->size >= st->max_size) {
+    return STACK_OVERFLOW;
+  }
+  st->size++;
+  st->data[st->size] = value;
+  return 0;
+}
+
+int pop(stack *st) {
+  if (st->size == -1) {
+    return STACK_UNDERFLOW;
+  }
+  int value = st->data[st->size];
+  st->size--;
+  return value;
+}
+
+int peek(stack *st) {
+  if (st->size == -1) {
+    return STACK_UNDERFLOW;
+  }
+  return st->data[st->size];
+}
+
+bool is_empty(stack *st) {
+  if (st->size == -1) {
+    return true;
+  } else {
+    return false;
+  }
+}
---
+++ stack_test.c
@@ -0,0 +1,58 @@
+#include "stack.h"
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define STACK_UNDERFLOW -100
+#define STACK_OVERFLOW -101
+
+void stack_underflow_test() {
+  stack *st = stack_init(2);
+  int res = pop(st);
+
+  assert(res == STACK_UNDERFLOW);
+}
+
+void stack_overflow_test() {
+  stack *st = stack_init(2);
+  push(st, 2);
+  push(st, 3);
+  int res = push(st,4);
+
+  assert(res == STACK_OVERFLOW);
+}
+
+void stack_pushpop_test() {
+  stack *st = stack_init(2);
+  push(st, 5);
+  int res = pop(st);
+  
+  assert(res == 5);
+}
+
+void stack_empty_test() {
+  stack *st = stack_init(2);
+  bool res = is_empty(st);
+
+  assert(res == true);
+}
+
+void stack_notempty_test() {
+  stack *st = stack_init(2);
+  push(st, 2);
+  bool res = is_empty(st);
+
+  assert(res == false);
+}
+
+int main() {
+
+  stack_underflow_test();
+  stack_overflow_test();
+  stack_pushpop_test();
+  stack_empty_test();
+  stack_notempty_test();
+  printf("stack success\n");
+
+  return 0;
+}

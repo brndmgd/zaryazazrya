ccb95730cbebd5cc79c9a337528be43b381b20dd
Revanshiest <emezhenov@gmail.com>
2025-03-06 00:12:57 +0600

    style: formatting

--- quadratic.c
+++ quadratic.c
@@ -1,25 +1,27 @@
-#include <math.h>
 #include "quadratic.h"
+#include <math.h>
 
 double is_zero(double x, double eps) {
-    if (x == -0 || fabs(x) < eps) return 0;
-    return x;
+  if (x == -0 || fabs(x) < eps)
+    return 0;
+  return x;
 }
 
-int roots(double a, double b, double c, double eps, double* x1, double* x2) {
-    
-    double D = b * b - 4 * a * c;
-    D = is_zero(D, 1e-7);
+int roots(double a, double b, double c, double eps, double *x1, double *x2) {
 
-    if (D < 0 || fabs(a) < 1e-7) return 0;
-    else if (fabs(D) < 1e-7) {
-        *x1 = is_zero(-b/(2*a), eps);
-        return 1;
-    }
-    else {
-        *x1 = (-b + sqrt(D))/(2*a);
-        *x2 = c / ( *x1 * a);
-        if(fabs(*x1 - *x2) < eps) return 1;
-        return 2;
-    }
+  double D = b * b - 4 * a * c;
+  D = is_zero(D, 1e-7);
+
+  if (D < 0 || fabs(a) < 1e-7)
+    return 0;
+  else if (fabs(D) < 1e-7) {
+    *x1 = is_zero(-b / (2 * a), eps);
+    return 1;
+  } else {
+    *x1 = (-b + sqrt(D)) / (2 * a);
+    *x2 = c / (*x1 * a);
+    if (fabs(*x1 - *x2) < eps)
+      return 1;
+    return 2;
+  }
 }
--- quadratic_test.c
+++ quadratic_test.c
@@ -1,65 +1,65 @@
-#include <stdio.h>
-#include <math.h>
 #include "quadratic.h"
 #include <assert.h>
+#include <math.h>
+#include <stdio.h>
 
 void square_test_1() {
-    double x1, x2;
-    double a = 1, b = 0, c = -1, eps = 1e-15;
-    int res = roots(a, b, c, eps, &x1, &x2);
+  double x1, x2;
+  double a = 1, b = 0, c = -1, eps = 1e-15;
+  int res = roots(a, b, c, eps, &x1, &x2);
 
-    assert(res == 2 && fabs(x1 - 1) < eps && fabs(x2 + 1) < eps);
+  assert(res == 2 && fabs(x1 - 1) < eps && fabs(x2 + 1) < eps);
 }
 
 void square_test_2() {
-    double x1, x2;
-    double a = 1, b = 0, c = 0, eps = 1e-15;
-    int res = roots(a, b, c, eps, &x1, &x2);
+  double x1, x2;
+  double a = 1, b = 0, c = 0, eps = 1e-15;
+  int res = roots(a, b, c, eps, &x1, &x2);
 
-    assert(res == 1 && fabs(x1) < eps);
+  assert(res == 1 && fabs(x1) < eps);
 }
 
 void square_test_3() {
-    double x1, x2;
-    double a = 1, b = 0, c = 1, eps = 1e-15;
-    int res = roots(a, b, c, eps, &x1, &x2);
+  double x1, x2;
+  double a = 1, b = 0, c = 1, eps = 1e-15;
+  int res = roots(a, b, c, eps, &x1, &x2);
 
-    assert(res == 0);
+  assert(res == 0);
 }
 
 void square_test_4() {
-    double x1, x2;
-    double a = 1, b = 0, c = -1e-7, eps = 1e-4;
-    int res = roots(a, b, c, eps, &x1, &x2);
+  double x1, x2;
+  double a = 1, b = 0, c = -1e-7, eps = 1e-4;
+  int res = roots(a, b, c, eps, &x1, &x2);
 
-    assert(res == 2 && fabs(x1-3e-4) < eps && fabs(x2+3e-4) < eps);
+  assert(res == 2 && fabs(x1 - 3e-4) < eps && fabs(x2 + 3e-4) < eps);
 }
 
 void square_test_5() {
-    double x1, x2;
-    double a = 1, b = -1e+10, c = -1, eps = 1e-11;
-    int res = roots(a, b, c, eps, &x1, &x2);
+  double x1, x2;
+  double a = 1, b = -1e+10, c = -1, eps = 1e-11;
+  int res = roots(a, b, c, eps, &x1, &x2);
 
-    assert(res == 2 && fabs(x1 - 1e+10) < eps && (x2 + 1e-10) < eps);
+  assert(res == 2 && fabs(x1 - 1e+10) < eps && (x2 + 1e-10) < eps);
 }
 
 void square_test_6() {
-    double x1, x2;
-    double a = 1, b = 0, c = -1e-8, eps = 1e-7;
-    int res = roots(a, b, c, eps, &x1, &x2);
+  double x1, x2;
+  double a = 1, b = 0, c = -1e-8, eps = 1e-7;
+  int res = roots(a, b, c, eps, &x1, &x2);
 
-    assert(res == 1 && fabs(x1) < eps);
+  assert(res == 1 && fabs(x1) < eps);
 }
 
 int main() {
 
-    square_test_1();
-    square_test_2();
-    square_test_3();
-    square_test_4();
-    square_test_5();
-    square_test_6();
-    printf("square success\n");
+  square_test_1();
+  square_test_2();
+  square_test_3();
+  square_test_4();
+  square_test_5();
+  square_test_6();
+  printf("square success\n");
 
-    return 0;
+  return 0;
 }

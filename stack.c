2aa62c15f43ef7753fec55d07cfeb68d33b3b6bf
brndmgd <brndmgdwastaken@gmail.com>
2025-03-12 21:37:39 +0600

    refactor: stack overflow and underflow flags, using size_t instead of int, memory managment

--- stack.c
+++ stack.c
@@ -1,44 +1,44 @@
 #include "stack.h"
-#include <stdbool.h>
-#include <stdlib.h>
-#define STACK_UNDERFLOW -100
-#define STACK_OVERFLOW -101
 
 stack *stack_init(int max_size) {
   stack *st = (stack *)malloc(sizeof(stack));
   st->data = malloc(sizeof(int) * max_size);
-  st->max_size = max_size - 1;
-  st->size = -1;
+  st->max_size = max_size;
+  st->size = 0;
+  st->overflow = false;
+  st->underflow = false;
   return st;
 }
 
 int push(stack *st, int value) {
   if (st->size >= st->max_size) {
+    st->overflow = true;
     return STACK_OVERFLOW;
   }
-  st->size++;
   st->data[st->size] = value;
+  st->size++;
   return 0;
 }
 
 int pop(stack *st) {
-  if (st->size == -1) {
+  if (st->size == 0) {
+    st->underflow = true;
     return STACK_UNDERFLOW;
   }
-  int value = st->data[st->size];
   st->size--;
-  return value;
-}
-
-int peek(stack *st) {
-  if (st->size == -1) {
-    return STACK_UNDERFLOW;
-  }
   return st->data[st->size];
 }
 
+int peek(stack *st) {
+  if (st->size == 0) {
+    st->underflow = true;
+    return STACK_UNDERFLOW;
+  }
+  return st->data[st->size-1];
+}
+
 bool is_empty(stack *st) {
-  if (st->size == -1) {
+  if (st->size == 0) {
     return true;
   } else {
     return false;

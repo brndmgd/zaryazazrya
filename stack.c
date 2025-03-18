3d8bc2f4c32ad747e82b019c2a6a079f47f2edcc
brndmgd <brndmgdwastaken@gmail.com>
2025-03-18 23:02:06 +0600

    feat:stack_free

--- stack.c
+++ stack.c
@@ -2,45 +2,50 @@
 
 stack *stack_init(int max_size) {
   stack *st = (stack *)malloc(sizeof(stack));
+  st->size = max_size;
+  st->top = 0;
   st->data = malloc(sizeof(int) * max_size);
-  st->max_size = max_size;
-  st->size = 0;
   st->overflow = false;
   st->underflow = false;
   return st;
 }
 
 int push(stack *st, int value) {
-  if (st->size >= st->max_size) {
+  if (st->top >= st->size) {
     st->overflow = true;
     return STACK_OVERFLOW;
   }
-  st->data[st->size] = value;
-  st->size++;
+  st->data[st->top] = value;
+  st->top++;
   return 0;
 }
 
 int pop(stack *st) {
-  if (st->size == 0) {
+  if (st->top == 0) {
     st->underflow = true;
     return STACK_UNDERFLOW;
   }
-  st->size--;
-  return st->data[st->size];
+  st->top--;
+  return st->data[st->top];
 }
 
 int peek(stack *st) {
-  if (st->size == 0) {
+  if (st->top == 0) {
     st->underflow = true;
     return STACK_UNDERFLOW;
   }
-  return st->data[st->size - 1];
+  return st->data[st->top - 1];
 }
 
 bool is_empty(stack *st) {
-  if (st->size == 0) {
+  if (st->top == 0) {
     return true;
   } else {
     return false;
   }
+}
+
+void stack_free(stack *st) {
+  free(st->data);
+  free(st);
 }

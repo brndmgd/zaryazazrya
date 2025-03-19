36ec0c07c01e4fdc80f4800c4f9ae8eb9524da34
Revanshiest <emezhenov@gmail.com>
2025-03-19 06:02:18 +0600

    feat: added error codes via #define

--- quadratic.c
+++ quadratic.c
@@ -13,15 +13,17 @@ int roots(double a, double b, double c, double eps, double *x1, double *x2) {
   D = is_zero(D, 1e-7);
 
   if (D < 0 || fabs(a) < 1e-7)
-    return 0;
+    return No_Substantial_Roots;
+  else if (fabs(a) < 1e-7)
+    return Not_Quadratic_Equation;
   else if (fabs(D) < 1e-7) {
     *x1 = is_zero(-b / (2 * a), eps);
-    return 1;
+    return One_Roots;
   } else {
     *x1 = (-b + sqrt(D)) / (2 * a);
     *x2 = c / (*x1 * a);
     if (fabs(*x1 - *x2) < eps)
-      return 1;
-    return 2;
+      return One_Roots;
+    return Two_Roots;
   }
 }

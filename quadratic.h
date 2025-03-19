37f4692d2e56af1484b525658687492f2991d385
Revanshiest <emezhenov@gmail.com>
2025-03-19 06:02:18 +0600

    feat: added error codes via #define

--- quadratic.h
+++ quadratic.h
@@ -1,5 +1,10 @@
 #include <math.h>
 
+#define No_Substantial_Roots -1
+#define Not_Quadratic_Equation -2
+#define One_Roots 1
+#define Two_Roots 2
+
 double is_zero(double x, double eps);
 
 int roots(double a, double b, double c, double eps, double *x1, double *x2);

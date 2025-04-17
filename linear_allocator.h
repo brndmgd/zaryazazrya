30ba1b7b791de48ccb6a4970f7481b91483add80
Daniil-Mazunin <dsss6872@gmail.com>
2025-04-17 21:54:05 +0600

    feat: linear allocator

---
+++ linear_allocator.h
@@ -0,0 +1,16 @@
+#include <stddef.h>
+#include <stdlib.h>
+
+typedef struct {
+  void *block_memory;
+  size_t size;
+  size_t used;
+} linear_allocator;
+
+linear_allocator *linear_allocator_init(size_t size);
+
+void *linear_alloc(linear_allocator *allocator, size_t size);
+
+void linear_reset(linear_allocator *allocator);
+
+void linear_free(linear_allocator *allocator);

77b890b3fa8f4d9a520f220246c2a9d3e30407fc
Michael <mishavarnavskiysek123@gmail.com>
2025-06-04 21:37:58 +0600

    feat: added the ability to use custom constructors and deconstructors

--- reference_counter.c
+++ reference_counter.c
@@ -1,33 +1,61 @@
+
 #include "reference_counter.h"
 
-ref_count_t *rc_init(void *obj, pool_allocator_t *allocator) {
-  if (obj == NULL || allocator == NULL)
+void default_dtor(void *object, pool_allocator_t *allocator) {
+  if (object) {
+    pool_free(allocator, object);
+  }
+}
+
+ref_count_t *rc_init(void *obj, pool_allocator_t *allocator, constructor ctor,
+                     deconstructor dtor) {
+  if (allocator == NULL) {
     return NULL;
+  }
 
   ref_count_t *rc = (ref_count_t *)pool_alloc(allocator);
-  if (rc == NULL)
+  if (rc == NULL) {
     return NULL;
+  }
 
   rc->count = 1;
-  rc->object = obj;
+
+  rc->ctor = ctor;
+
+  if (ctor) {
+    rc->object = ctor(allocator);
+    if (rc->object == NULL) {
+      pool_free(allocator, rc);
+      return NULL;
+    }
+  } else {
+    rc->object = obj;
+  }
+  rc->dtor = (dtor != NULL ? dtor : default_dtor);
+
   return rc;
 }
 
 void rc_increase(ref_count_t *rc) {
-  if (rc)
-    rc->count++;
+  if (rc) {
+    rc->count += 1;
+  }
 }
 
 void rc_decrease(ref_count_t *rc, pool_allocator_t *allocator) {
-  if (rc)
-    rc->count--;
-  if (rc->count == 0) {
-    if (rc->object == NULL) {
-      pool_free(allocator, rc);
-      return;
-    }
+  if (rc == NULL || allocator == NULL) {
+    return;
+  }
 
-    pool_free(allocator, rc->object);
+  if (rc->count == 0) {
+    return;
+  }
+
+  rc->count -= 1;
+  if (rc->count == 0) {
+    if (rc->dtor) {
+      rc->dtor(rc->object, allocator);
+    }
     pool_free(allocator, rc);
   }
 }
--- reference_counter.h
+++ reference_counter.h
@@ -1,11 +1,19 @@
 #include "pool_allocator.h"
 #include <stdlib.h>
 
+typedef void (*deconstructor)(void *object, pool_allocator_t *allocator);
+typedef void *(*constructor)(pool_allocator_t *allocator);
+
 typedef struct {
   size_t count;
   void *object;
+  deconstructor dtor;
+  constructor ctor;
 } ref_count_t;
 
-ref_count_t *rc_init(void *obj, pool_allocator_t *allocator);
+ref_count_t *rc_init(void *obj, pool_allocator_t *allocator, constructor ctor,
+                     deconstructor dtor);
 void rc_increase(ref_count_t *rc);
 void rc_decrease(ref_count_t *rc, pool_allocator_t *allocator);
+void default_dtor(void *object, pool_allocator_t *allocator);
+

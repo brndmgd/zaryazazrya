ca62a1aaf96e5411ab748d61ef72e662c1a2cac5
Daniil-Mazunin <dsss6872@gmail.com>
2025-06-05 01:47:09 +0600

    feat: added cyclic link detection

--- reference_counter_test.c
+++ reference_counter_test.c
@@ -85,11 +85,61 @@ void default_dtor_and_ctor_test() {
   pool_deinit(allocator);
 }
 
+void create_cycle_reference_test() {
+  pool_allocator_t *allocator = pool_init(10, sizeof(ref_count_t));
+  assert(allocator != NULL);
+  cycle_object *obj1 = create_cycle_object(allocator);
+  cycle_object *obj2 = create_cycle_object(allocator);
+  assert(obj1 != NULL);
+  assert(obj2 != NULL);
+  assert(obj1->rc != NULL);
+  assert(obj2->rc != NULL);
+  assert(obj1->rc->count == 1);
+  assert(obj2->rc->count == 1);
+  create_cycle_reference(obj1, obj2);
+  assert(obj1->ref_to == obj2);
+  assert(obj2->ref_to == obj1);
+  assert(obj1->rc->count == 2);
+  assert(obj2->rc->count == 2);
+  assert(detected_cycle(obj1) == 1);
+  assert(detected_cycle(obj2) == 1);
+  delete_cycle(allocator, obj1);
+  pool_deinit(allocator);
+}
+
+void detected_cycle_test() {
+  pool_allocator_t *allocator = pool_init(10, sizeof(ref_count_t));
+  assert(allocator != NULL);
+  cycle_object *obj1 = create_cycle_object(allocator);
+  cycle_object *obj2 = create_cycle_object(allocator);
+  cycle_object *obj3 = create_cycle_object(allocator);
+  assert(obj1 != NULL);
+  assert(obj2 != NULL);
+  assert(obj3 != NULL);
+  obj1->ref_to = obj2;
+  rc_increase(obj2->rc);
+  obj2->ref_to = obj3;
+  rc_increase(obj3->rc);
+  obj3->ref_to = obj1;
+  rc_increase(obj1->rc);
+  assert(detected_cycle(obj1) == 1);
+  assert(detected_cycle(obj2) == 1);
+  assert(detected_cycle(obj3) == 1);
+  cycle_object *obj4 = create_cycle_object(allocator);
+  assert(obj4 != NULL);
+  assert(detected_cycle(obj4) == 0);
+  delete_cycle(allocator, obj1);
+  rc_decrease(obj4->rc, allocator);
+  pool_deinit(allocator);
+}
+
 int main() {
   create_reference_test();
   increase_reference_test();
   decrease_reference_test();
   custom_dtor_and_ctor_test();
   default_dtor_and_ctor_test();
+  create_cycle_reference_test();
+  detected_cycle_test();
   return 0;
 }
